b0420acdd932a5b88798860d01298b0454f288c5
Murshid K M <A-10347@LE1887.local>
2025-06-20 01:43:14 +0530

    copyrigths updates

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -184,7 +184,7 @@ const Home = () => {
           <i className="bi bi-instagram" />
           <i className="bi bi-github" />
         </div>
-        <p>murshidwork@email.com</p>
+        <p>murshidwork@gmail.com</p>
         <p>Copyright © 2020 Murshid KM. All rights reserved.</p>
       </footer>
     </div>
